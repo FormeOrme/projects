92a1aa5bdeefe36a7b6c71a79dcd5275f3951394
federico.formenti <federico.formenti.0@gmail.com>
2017-12-29 16:15:17 +0100

    added Reaction Diffusion

---
+++ reaction_diffusion/r_d.js
@@ -0,0 +1,126 @@
+var cells = new Array();
+var SIDE = 20;
+var MIN_COLOR;
+var MAX_COLOR;
+var COLORS = new Array();
+
+var C_HEIGTH;
+var C_WIDTH;
+
+var dA = 1;
+var dB = 0.5;
+var feed = 0.055;
+var k = 0.062;
+
+function setup() {
+	var myCanvas = createCanvas(windowWidth, windowHeight);
+	myCanvas.parent('canvasContainer');
+	frameRate(60);
+	
+	C_HEIGTH = Math.ceil(windowHeight/SIDE);
+	C_WIDTH  = Math.ceil(windowWidth/SIDE);;
+    
+	for(var y = 0; y < C_HEIGTH; y ++){
+		for(var x = 0; x < C_WIDTH; x ++){
+			cells.push(new Cell(x, y, 1, 0));
+		}
+	}
+    
+    cells.filter(function(f){ return f.x > 10 && f.x < 14 && f.x > 5 && f.x < 6; }).map(function(f){
+        f.b = 1;
+    });
+    
+	noStroke();
+	
+	MAX_COLOR = color(random(0, 100), random(0, 100), random(0, 100));
+	MIN_COLOR = color(random(100, 255), random(100, 255), random(100, 255));
+}
+
+function getColor(a, b){
+    var red = map(a, 0, a+b, MIN_COLOR._getRed(), MAX_COLOR._getRed());
+    var blu = map(a, 0, a+b, MIN_COLOR._getBlue(), MAX_COLOR._getBlue());
+    var gre = map(a, 0, a+b, MIN_COLOR._getGreen(), MAX_COLOR._getGreen());
+    
+    return color(red, blu, gre);
+}
+
+var tempCell;
+function draw() {
+	tempCell = JSON.parse(JSON.stringify(cells));
+	
+	cells.map(function(c){
+		c.update(tempCell);
+	});
+    
+    cells.map(function(c){
+		c.draw();
+	});
+}
+
+function Cell(x, y, a, b) {
+	this.id = x + y*SIDE;
+	this.x = x;
+	this.y = y;
+    
+    this.a = a;
+    this.b = b;
+    
+	this.draw = function(){
+		fill(getColor(this.a, this.b));
+		rect(this.x*SIDE, this.y*SIDE, SIDE-1, SIDE-1);
+	}
+	
+	this.nbrs = getNbrs(x, y);
+	
+	this.update = function(cells){
+        this.a = constrain(a + (dA * laplace(cells, x, y, 'a')) - (a * b * b) + (feed * (1 - a)), 0, 1);
+        this.b = constrain(b + (dB * laplace(cells, x, y, 'b')) + (a * b * b) - ((k + feed) * b), 0, 1);
+	}
+}
+
+function laplace(cells, x, y, v) {
+	var sum = 0;
+	var t;
+	sum += !!(t = getCell(cells, x    , y    ))?t[v]:0 * -1;
+	sum += !!(t = getCell(cells, x - 1, y    ))?t[v]:0 * 0.2;
+	sum += !!(t = getCell(cells, x + 1, y    ))?t[v]:0 * 0.2;
+	sum += !!(t = getCell(cells, x    , y + 1))?t[v]:0 * 0.2;
+	sum += !!(t = getCell(cells, x    , y - 1))?t[v]:0 * 0.2;
+	sum += !!(t = getCell(cells, x - 1, y - 1))?t[v]:0 * 0.05;
+	sum += !!(t = getCell(cells, x + 1, y - 1))?t[v]:0 * 0.05;
+	sum += !!(t = getCell(cells, x + 1, y + 1))?t[v]:0 * 0.05;
+	sum += !!(t = getCell(cells, x - 1, y + 1))?t[v]:0 * 0.05;
+	return sum;
+}
+function getCell(cells, x, y){
+    return cells.filter(function(f){ return f.x == x && f.y == y })[0];
+}
+
+function getNbrs(x, y){
+	nbrs = new Array();
+	if(x != 0){
+		nbrs.push( x-1 + (y-1) * C_WIDTH );
+		nbrs.push( x-1 + y   * C_WIDTH );
+		nbrs.push( x-1 + (y+1) * C_WIDTH );
+	} else {
+		nbrs.push( C_WIDTH-1 + (y-1) * C_WIDTH );
+		nbrs.push( C_WIDTH-1 + y   * C_WIDTH );
+		nbrs.push( C_WIDTH-1 + (y+1) * C_WIDTH );
+	}
+	nbrs.push( x + (y-1) * C_WIDTH );
+	nbrs.push( x + (y+1) * C_WIDTH );
+	if(x != C_WIDTH-1){
+		nbrs.push( x+1 + (y-1) * C_WIDTH );
+		nbrs.push( x+1 + y   * C_WIDTH );
+		nbrs.push( x+1 + (y+1) * C_WIDTH );
+	} else {
+		nbrs.push( 0 + (y-1) * C_WIDTH );
+		nbrs.push( 0 + y   * C_WIDTH );
+		nbrs.push( 0 + (y+1) * C_WIDTH );
+	}
+	return nbrs.filter(function(n){return n >= 0 && n < C_HEIGTH*C_WIDTH});
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
