ecffbec9c416ce00fa78e2baf1ca999893c4ee4f
federico.formenti <federico.formenti.0@gmail.com>
2018-02-08 17:14:59 +0100

    Updated Cover

--- skin/script.js
+++ skin/script.js
@@ -2,11 +2,25 @@ var $skin;
 
 $(document).ready(function(){
     $skin = $('#skin');
-    
-    $('.face').map(function(i, o){
-        $(o).css("background-image", "url("+$skin.attr('src')+")");
-    });
-    
+    update();
+})
+
+function update(){
+    //$('.face').map(function(i, o){
+    //    $(o).css("background-image", "url("+$skin.attr('src')+")");
+    //});
     //sheet.insertRule( '.face{ background-image: url('+$skin.attr('src')+') }' );
     //sheet.addRule( '.face', 'background-image: url('+$skin.attr('src')+')');
+    if(!!document.styleSheets[0].addRule){
+        document.styleSheets[0].addRule( '.face', 'background-image: url('+$skin.attr('src')+')');
+    } else {
+        document.styleSheets[0].insertRule( '.face{ background-image: url('+$skin.attr('src')+') }' );
+    }
+}
+
+var rotate = true;
+
+$(document).on('click', '.toggle', function(){
+    var $class = $(this).data('class');
+    $('.'+$class).toggle();
 })
