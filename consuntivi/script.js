5cb16a120eef16052c0c5ffe3bbaac6818c885fe
FormeOrme <34321372+FormeOrme@users.noreply.github.com>
2018-03-07 00:09:39 +0100

    Update Selection

--- consuntivi/script.js
+++ consuntivi/script.js
@@ -1,3 +1,4 @@
+/* COMMANDS SECTION */
 $(document).ready(function(){
     $('.command.empty').hide();
 });
@@ -20,29 +21,44 @@ $(document).on('click', '#commands .close', function(){
     $('#commands').toggleClass('closed');
 });
 
-$(document).on('click', '.activity', function(e){
-    e.stopPropagation();
-    $(this).toggleClass('selected');
-});
-
+/* SELECTION */
 var selecting = false;
 $(document).on('mousedown', '.day:not(.selected)', function(){
-    selecting = true;
+    selecting = 'day';
     $(this).addClass('selecting');
 });
 $(document).on('mouseenter', '.day:not(.selected)', function(){
-    if(selecting){
+    if(selecting=='day'){
         $(this).addClass('selecting');
     }
 });
+$(document).on('mousedown', '.activity', function(e){
+    e.stopPropagation();
+});
+$(document).on('mousedown', '.activity:not(.selected)', function(){
+    selecting = 'activity';
+    $(this).addClass('selecting');
+});
+$(document).on('mouseenter', '.activity:not(.selected)', function(){
+    if(selecting=='activity'){
+        $(this).addClass('selecting');
+    }
+});
+
 var unselecting = false;
 $(document).on('mousedown', '.selected', function(){
-    unselecting = true;
-    $(this).addClass('unselecting');
+    var $this = $(this);
+    if($this.is('.day')){
+        unselecting = 'day';
+    } else if ($this.is('.activity')){
+        unselecting = 'activity';
+    }
+    $this.addClass('unselecting');
 });
 $(document).on('mouseenter', '.selected', function(){
-    if(unselecting){
-        $(this).addClass('unselecting');
+    var $this = $(this);
+    if($this.hasClass(unselecting)){
+        $this.addClass('unselecting');
     }
 });
 
