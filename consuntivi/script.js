d060d53664a2057263637b77f8489e55b6ce28dc
federico.formenti <federico.formenti.0@gmail.com>
2018-03-06 18:37:27 +0100

    Added Drag and select

--- consuntivi/script.js
+++ consuntivi/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
 $(document).on('click', '.clean', function(){
     $('.selected').removeClass('selected');
+    $('.selecting').removeClass('selecting');
 });
 
 $(document).on('click', '.toggle', function(){
@@ -19,8 +20,35 @@ $(document).on('click', '#commands .close', function(){
     $('#commands').toggleClass('closed');
 });
 
-$(document).on('click', '.day, .activity', function(e){
+$(document).on('click', '.activity', function(e){
     e.stopPropagation();
     $(this).toggleClass('selected');
 });
 
+var selecting = false;
+$(document).on('mousedown', '.day:not(.selected)', function(){
+    selecting = true;
+    $(this).addClass('selecting');
+});
+$(document).on('mouseenter', '.day:not(.selected)', function(){
+    if(selecting){
+        $(this).addClass('selecting');
+    }
+});
+var unselecting = false;
+$(document).on('mousedown', '.selected', function(){
+    unselecting = true;
+    $(this).addClass('unselecting');
+});
+$(document).on('mouseenter', '.selected', function(){
+    if(unselecting){
+        $(this).addClass('unselecting');
+    }
+});
+
+$(document).on('mouseup', function(){
+    $('.selecting').removeClass('selecting').addClass('selected');
+    $('.unselecting').removeClass('selected').removeClass('unselecting');
+    unselecting = false;
+    selecting = false;
+});
