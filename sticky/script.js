3cd3f8b78c2984d6cc845ec681533aa769e6d755
Federico Formenti <federico.formenti.0@gmail.com>
2018-10-17 18:30:56 +0200

    update sticky

--- sticky/script.js
+++ sticky/script.js
@@ -1,3 +1,12 @@
+/* SETUP */
+
+var wrap = document.getElementsByClassName('wrap-text');
+for (var i = 0; i < wrap.length; i++) {
+	wrap[i].innerHTML = wrap[i].innerHTML.replace(/(\w+)/g, '<span class="block">$1</span>');
+}
+
+/* EVENTS */
+
 var body = document.getElementsByTagName("BODY")[0];
 var toggle3D = document.getElementById('toggle3D');
 
