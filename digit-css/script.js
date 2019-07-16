4f7dd00fa93ab5cfcaedc55129cf4b0b41a346d2
Federico Formenti <federico.formenti.0@gmail.com>
2019-07-16 18:08:53 +0200

    Aggiornati progetti

--- digit-css/script.js
+++ digit-css/script.js
@@ -1,12 +1,49 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
+// ⠁_⠂_⠐_⠈_⠃_⠘_⠉_⠒_⠋_⠓_⠙_⠚_⠛
+const NUMS = [
+    "⠐⠒⠒⠂"+
+    "⠘⠋⠙⠃"+
+    "⠘⠃⠘⠃"+
+    "⠘⠃⠘⠃"+
+    "⠘⠓⠚⠃"+
+    "⠈⠉⠉⠁"
+];
+
 function init(){
-    const digit = document.querySelector('.digit');
     let counter = 0;
+
+    const digit = buildDigit("digit");
+    printDigit(digit, counter);
+
     document.addEventListener('click', function (e) {
-        digit.classList.remove('n'+counter);
         //counter = ++counter%10;
-        counter = ++counter%2;
-        digit.classList.add('n'+counter);
+        counter = ++counter%10;
+        printDigit(digit, counter);
     });
+}
+
+const printDigit = ( d, count )=>{
+    d.forEach((c, i) => {
+        c.classList = [];
+        c.classList.add("cell");
+        c.classList.add(NUMS[count].split()[i]||"X");
+    });
+}
+
+const buildDigit = (id) => {
+    var arr = [];
+    var digit = document.getElementById(id);
+    for(var r = 0; r < 6; r++){
+        var row = document.createElement("span");
+        row.classList.add("row");
+        for(var c = 0; c < 4; c++){
+            var cell = document.createElement("span");
+            cell.classList.add("cell");
+            row.append(cell);
+            arr.push(cell);
+        }
+        digit.append(row);
+    }
+    return arr;
 }
--- screenshot/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', init, false);
-
-function init(){
-    
-}
