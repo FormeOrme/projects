bac6281fcfa412eff40d1d71242a515a3ab7f0b0
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2022-10-11 16:49:15 +0200

    Aggiunto mtg_collection

--- tools/common.js
+++ tools/common.js
@@ -1,8 +1,15 @@
+/* SHORTHANDS */
+const gid = document.getElementById.bind(document);
+const gq = document.querySelector.bind(document);
+const gqa = document.querySelectorAll.bind(document);
+
 /* STRING TO HSL COLOR */
 const toH = (str, l = 72, s = 65, d = 210, k = 6, n = 13) =>
     `hsla(${(Array.from(str).reduce((a, c, i) => a + c.charCodeAt() * n * (k + i), d) % 360)}, ${l}%, ${s}%, 1)`
 
 const UUID = () => self.crypto.randomUUID();
+
+const isHtml = s => s.includes("<") && (!s.includes("{") || (s.includes("{") && (s.indexOf("{") > s.indexOf("<"))))
 
 /* ADD STYLE NODE UTIL */
 const addStyleNode = (s) => {
@@ -18,7 +25,7 @@ Storage.prototype.setObject = function (n, v) { this.setItem(n, JSON.stringify(v
 
 /* CREATE DOM ELEMENT */
 const createElement = (e) => {
-    const node = document.createElement(e.type);
+    const node = document.createElement(!!e.type ? e.type : "div");
     !e.id || (node.id = e.id);
     !e.children ||
         e.children.forEach((c) => {
@@ -26,12 +33,14 @@ const createElement = (e) => {
             node.appendChild(node[c.class]);
         });
     !e.innerText || (node.innerText = e.innerText);
-    !e.class || node.classList.add(...e.class);
-    !e.attrs || e.attrs.forEach((a) => node.setAttribute(a[0], a[1]));
-
+    if (!!e.class) {
+        Array.isArray(e.class) ? node.classList.add(...e.class) : node.classList.add(...e.class.trim().split(" "))
+    }
+    !e.attrs || Object.entries(e.attrs).forEach(([k, v]) => node.setAttribute(k, v));
     /* EVENTS */
-    !e.events || Object.entries(e.events).map(([k, v]) => node.addEventListener(k, (e) => v(e, node), false));
-
+    if (!!e.events) {
+        Object.entries(e.events).forEach(([k, v]) => node.addEventListener(k, (e) => v(e, node), false));
+    }
     /* CUSTOM PROPERTIES - OBJECT */
     node.custom = e.custom;
 
@@ -52,4 +61,28 @@ const getShuffledArr = arr => {
         [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
     }
     return newArr
+}
+
+function csvJSON(text, quoteChar = '"', delimiter = ',') {
+    var rows = text.split("\n");
+    var headers = rows[0].split(",");
+
+    const regex = new RegExp(`\\s*(${quoteChar})?(.*?)\\1\\s*(?:${delimiter}|$)`, 'gs');
+
+    const match = line => [...line.matchAll(regex)]
+        .map(m => m[2])
+        .slice(0, -1);
+
+    var lines = text.split('\n');
+    const heads = headers ?? match(lines.shift());
+    lines = lines.slice(1);
+
+    return lines.map(line => {
+        return match(line).reduce((acc, cur, i) => {
+            // replace blank matches with `null`
+            const val = cur.length <= 0 ? null : Number(cur) || cur;
+            const key = heads[i] ?? `{i}`;
+            return { ...acc, [key]: val };
+        }, {});
+    });
 }
