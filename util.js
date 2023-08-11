5220aef0c7e4a33517295272cdab2bbd24c3b99d
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2023-08-11 15:54:35 +0200

    Update util.js

--- util.js
+++ util.js
@@ -1,4 +1,5 @@
 class Utils {
+    static PRC = (w, m) => m / 100 * w;
 	static toX = (i, w) => i % w
 	static toY = (i, w) => ~~(i / w)
 	static toXY = (i, w) => ({ x: toX(i, w), y: toY(i, w) })
@@ -6,6 +7,21 @@ class Utils {
 	static toID_O = (xy, w) => xy.y * w + xy.x
 	static toID_A = (xy, w) => xy[1] * w + xy[0]
 	static clone = o => JSON.parse(JSON.stringify(o))
+	static createElement = (e) => {
+		const node = document.createElement(e.type);
+		!e.id || (node.id = e.id);
+		!e.children ||
+			e.children.forEach((c) => {
+				node[c.class] = Utils.createElement(c);
+				node.appendChild(node[c.class]);
+			});
+		!e.innerText || (node.innerText = e.innerText);
+		!e.class || node.classList.add(...e.class);
+		!e.attrs || e.attrs.forEach((a) => node.setAttribute(a[0], a[1]));
+		!e.events || Object.entries(e.events).map(([k, v]) => node.addEventListener(k, (e) => v(e, node), false));
+		node.custom = e.custom;
+		return node;
+	};
 
 	static monitor(parent, target, callback) {
 		var mObs = new window.MutationObserver(() => {
