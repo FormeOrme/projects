5128d251f6752fcfa8f91ba198ad01b1d7a365c6
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2023-08-26 23:54:20 +0200

    Update util.js

--- util.js
+++ util.js
@@ -1,24 +1,42 @@
+
 class Utils {
-	static PRC = (w, m) => m / 100 * w;
+	/* STRING TO HSL COLOR */
+	static toH = (s, d = 210, k = 6, n = 13) => `hsla(${(Array.from(s).reduce((a, c, i) => a + c.charCodeAt() * n * (k + i), d) % 360)}, 72%, 65%, 1)`;
+
+	/* ADD STYLE NODE UTIL */
+	static addStyleNode = (s) => {
+		let style = document.createElement('style');
+		document.head.appendChild(style);
+		style.appendChild(document.createTextNode(s));
+	}
+
+	static prc = (current, max) => Utils.normalize(current, max) * 100;
+	static normalize = (current, max) => current / max;
+
 	static toX = (i, w) => i % w;
 	static toY = (i, w) => ~~(i / w);
 	static toXY = (i, w) => ({ x: toX(i, w), y: toY(i, w) });
 	static toID = (x, y, w) => y * w + x;
-	static toID_O = (xy, w) => xy.y * w + xy.x;
-	static toID_A = (xy, w) => xy[1] * w + xy[0];
-	static clone = o => JSON.parse(JSON.stringify(o));
+	static toID_O = (xy, w) => toID(xy.x, xy.y, w);
+	static toID_A = (xy, w) => toID(xy[0], xy[1], w);
+
+	static clone = o => Object.setPrototypeOf(JSON.parse(JSON.stringify(o)), o.constructor.prototype);
 	static createElement = (e) => {
 		const node = document.createElement(e.type);
 		!e.id || (node.id = e.id);
-		!e.children ||
-			e.children.forEach((c) => {
-				node[c.class] = Utils.createElement(c);
-				node.appendChild(node[c.class]);
-			});
 		!e.innerText || (node.innerText = e.innerText);
-		!e.class || node.classList.add(...e.class);
-        	!e.attrs || Object.entries(e.attrs).forEach(([k, v]) => node.setAttribute(k, v));
-		!e.events || Object.entries(e.events).forEach(([k, v]) => node.addEventListener(k, (e) => v(e, node), false));
+		!e.value || (node.value = e.value);
+		if (!!e.children) {
+			(Array.isArray(e.children) ? e.children : [e.children]).forEach((c) => {
+				node.appendChild(Utils.createElement(c));
+			});
+		}
+		if (!!e.class) {
+			e.class = Array.isArray(e.class) ? e.class : e.class.trim().split(" ");
+			node.classList.add(...e.class);
+		}
+		!e.attribute || Object.entries(e.attribute).forEach(([k, v]) => node.setAttribute(k, v));
+		!e.event || Object.entries(e.event).forEach(([k, v]) => node.addEventListener(k, (e) => v(e, node), false));
 		node.custom = e.custom;
 		return node;
 	};
@@ -38,8 +56,41 @@ class Utils {
 		window.addEventListener("animationend", e => { observe(); });
 	}
 
-	static event = new Event("utils-loaded");
+	static UTILS_LOADED = "utils-loaded";
 	static loaded() {
-		document.dispatchEvent(Utils.event);
+		document.dispatchEvent(new Event(UTILS_LOADED));
+	}
+
+	static Elem = class {
+		get type() {
+			return this.constructor.name;
+		}
+		static with(obj) {
+			return Object.assign(eval(`new ${this.name}()`), obj);
+		}
+		static create() {
+			return this.with().create();
+		}
+		create() {
+			return Utils.createElement(this);
+		}
+		toJSON() {
+			return {
+				type: this.type,
+				...this
+			}
+		}
 	}
 }
+
+class Table extends Utils.Elem { }
+class THead extends Utils.Elem { }
+class TBody extends Utils.Elem { }
+class TFoot extends Utils.Elem { }
+class TR extends Utils.Elem { }
+class TD extends Utils.Elem { }
+class Div extends Utils.Elem { }
+class Input extends Utils.Elem { }
+class Span extends Utils.Elem { }
+class Button extends Utils.Elem { }
+class TextArea extends Utils.Elem { }
