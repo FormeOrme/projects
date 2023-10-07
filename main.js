39d3fb05c36e1005df1ede58391a3cbfd838e180
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2023-10-07 16:33:03 +0200

    Added Bootstrap to index

--- main.js
+++ main.js
@@ -7,17 +7,24 @@ const link = (path) => `https://formeorme.github.io/projects/${path}/`;
 Utils.fetchJson({
     url: apiUrl
 }).then(response => {
-    Dom.qs("body").append(Menu.with({
-        children: response.json
-            .filter(r => r.size === 0)
-            .sort((r1, r2) => r1.path.localeCompare(r2.path))
-            .map(r => LI.with({
-                children: A.with({
-                    attribute: {
-                        href: link(r.path)
-                    },
-                    innerText: r.path
-                })
-            }))
+
+    Dom.qs("body").append(Div.with({
+        class: "container",
+        children: Span.with({
+            children: response.json
+                .filter(r => r.size === 0)
+                .sort((r1, r2) => r1.path.localeCompare(r2.path))
+                .map(r => Small.with({
+                    class: "d-inline-flex",
+                    children: A.with({
+                        class: "btn btn-light m-3",
+                        attribute: {
+                            href: link(r.path)
+                        },
+                        innerText: r.path
+                    })
+                }))
+        })
     }).create());
+
 })
