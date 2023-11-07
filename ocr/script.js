c9c9d6bf976b4a00812660e5fbb77287eb2c3a6d
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2023-11-07 11:14:02 +0100

    Update script.js

--- ocr/script.js
+++ ocr/script.js
@@ -5,26 +5,41 @@ document.querySelector("body").append(Div.with({
             innerText: "OCR"
         }),
         Input.with({
-            class: "form-control form-control-sm",
+            class: "form-control form-control-sm mb-1",
             attribute: {
-                type: "file"
+                type: "file",
+                title: "input"
             },
             event: {
                 input: (e, node) => {
-                    tesseractReadFile(node.files[0])
+                    parse(node.files[0])
                 }
             }
         }),
         Div.with({
             id: "progressContainer",
-            class: "progress mb-1",
+            class: "progress mb-1 position-relative",
             attribute: {
                 style: "height: 30px"
             },
-            children: Div.with({
-                id: "progressPercent",
-                class: "progress-bar"
-            })
+            children: [
+                Div.with({
+                    id: "progressPercent",
+                    class: "progress-bar",
+                    function: {
+                        show: function () { this.classList.remove(Utils.hideClass); },
+                        hide: function () { this.classList.add(Utils.hideClass); },
+                    },
+                }),
+                Div.with({
+                    id: "progressSpinner",
+                    class: "spinner-grow text-primary position-absolute top-50 start-50 translate-middle",
+                    function: {
+                        show: function () { this.classList.remove(Utils.hideClass); },
+                        hide: function () { this.classList.add(Utils.hideClass); },
+                    },
+                })
+            ]
         }),
         Pre.with({
             class: "form-control form-control-sm",
@@ -32,25 +47,24 @@ document.querySelector("body").append(Div.with({
         })
     ]
 }).create());
+Dom.NODES.progressSpinner.hide();
 
 
-function tesseractReadFile(file) {
-    try {
-        Tesseract.recognize(
-            file,
-            'eng',
-            {
-                logger: l => {
-                    const progress = !l.jobId ? 0 : l.progress;
-                    Dom.NODES.progressPercent.style = `width: ${(progress * 100).toFixed(0)}%`;
-                }
+function parse(file) {
+    Dom.NODES.progressPercent.hide();
+    Dom.NODES.progressSpinner.show();
+    Tesseract.recognize(file, 'eng', {
+        logger: l => {
+            const progress = !l.jobId ? 0 : l.progress;
+            if (progress != 0) {
+                Dom.NODES.progressSpinner.hide();
+                Dom.NODES.progressPercent.show();
             }
-        ).then(({ data: { text } }) => {
-            Dom.NODES.output.innerText = text;
-        });
-    } catch (e) {
-        console.log(e);
-    }
+            Dom.NODES.progressPercent.style = `width: ${(progress * 100).toFixed(0)}%`;
+        }
+    }).then(({ data: { text } }) => {
+        Dom.NODES.output.innerText = text;
+    });
 }
 
 
@@ -60,8 +74,7 @@ document.querySelector("body").addEventListener('paste', (event) => {
 
     for (let i = 0; i < items.length; i++) {
         if (items[i].type.indexOf('image') !== -1) {
-            const blob = items[i].getAsFile();
-            tesseractReadFile(blob);
+            parse(items[i].getAsFile());
         }
     }
 });
