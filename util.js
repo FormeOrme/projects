5e418c8d904ba9f09e8cde15de0b7c5c766d6d80
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2023-12-13 12:39:34 +0100

    Update util.js

--- util.js
+++ util.js
@@ -90,8 +90,8 @@ class Utils {
 }
 
 class Filter {
-	static first = (o, i) => i !== 0;
-	static last = (o, i, a) => i !== a.length - 1;
+	static first = (o, i) => i === 0;
+	static last = (o, i, a) => i === a.length - 1;
 	static notNull = Boolean;
 }
 
