27900d5869f74a09669a6d22bf7de47e037bb02c
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2025-04-15 21:29:39 +0200

    Update table.js

--- table.js
+++ table.js
@@ -1,12 +1,17 @@
 class TableBuilder {
 
-    setHeaders(columns) {
+    setColumns(columns) {
         this.columns = columns;
         return this;
     }
 
     setData(data) {
         this.data = data;
+        return this;
+    }
+
+    setClasses(classes) {
+        this.classes = classes;
         return this;
     }
 
@@ -20,35 +25,52 @@ class TableBuilder {
     build() {
         return Span.with({
             id: "table",
-            class: "d-flex flex-column m-2 border border-2 rounded-3 shadow-sm overflow-hidden",
+            class: [
+                "d-flex flex-column",
+                this.classes?.table
+            ],
             children: [
                 Span.with({
                     id: "header",
-                    class: "d-flex flex-column bg-body-tertiary",
+                    class: [
+                        "d-flex flex-column",
+                        this.classes?.header
+                    ],
                     children: Span.with({
                         id: "header-row",
-                        class: "d-flex flex-row",
+                        class: [
+                            "d-flex flex-row",
+                            this.classes?.headerRow
+                        ],
                         children: Object.keys(this.columns).map((key, x) => Span.with({
                             id: `header-${key}`,
                             class: [
+                                this.classes?.headerCell,
                                 x !== 0 ? "border-start" : "",
                                 this.columns[key].class ?? ""
                             ],
-                            children: this.buildCell(this.columns[key].label, "fw-bold")
+                            children: this.buildCell(this.columns[key].label)
                         }))
                     })
                 }),
                 Span.with({
                     id: "body",
-                    class: "d-flex flex-column",
+                    class: [
+                        "d-flex flex-column",
+                        this.classes?.body
+                    ],
                     children: this.data.map((row, y) => Span.with({
                         id: `row-${y}`,
-                        class: "d-flex flex-row border-top",
+                        class: [
+                            "d-flex flex-row",
+                            this.classes?.row,
+                        ],
                         children: Object.keys(columns).map((key, x) => {
                             const column = this.columns[key];
                             return Span.with({
                                 id: `cell-${y}-${key}`,
                                 class: [
+                                    this.classes?.cell,
                                     x !== 0 ? "border-start" : "",
                                     column.class ?? ""
                                 ],
