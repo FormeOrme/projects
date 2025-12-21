c4857316f099502e759ef006b96be6001e79942d
Federico Formenti <federico.formenti.0@gmail.com>
2025-12-21 14:03:43 +0100

    Add prices page and JSON data for game pricing information
    - Created index.html for displaying game prices with a structured table.
    - Implemented JavaScript classes for handling price data and calculations.
    - Added prices.json containing detailed pricing information for multiple games.

--- module/Dom.js
+++ module/Dom.js
@@ -13,6 +13,108 @@ class Nodes extends Array {
     }
 }
 
+function createElement(e, namespace) {
+    namespace = namespace || e.namespace;
+    const type = e.constructor.name.replace("_", "").toLowerCase();
+    const node = namespace
+        ? document.createElementNS(namespace, type)
+        : document.createElement(type);
+
+    e.node = node;
+    node.source = e;
+
+    if (e.id) node.id = e.id;
+    if (e.innerText !== undefined) node.textContent = e.innerText;
+    if (e.text !== undefined) node.textContent = e.text;
+    if (e.value !== undefined) node.value = e.value;
+    if (e.type) node.type = e.type;
+    if (e.editable) node.contentEditable = e.editable;
+
+    if (e.class) {
+        node.className = compact(e.class).join(" ").trim();
+    }
+    if (e.event) {
+        for (let k in e.event) {
+            node.addEventListener(k, e.event[k].bind(node, node), false);
+        }
+    }
+    if (e.attribute) {
+        Dom.formatKeys(e.attribute).forEach(([k, v]) => node.setAttribute(k, v));
+    }
+    if (e.style) {
+        Dom.formatKeys(e.style).forEach(([k, v]) =>
+            node.style.setProperty(k, ...(v.split ? v.split("!") : [v])),
+        );
+    }
+    if (e.function) {
+        Object.entries(e.function).forEach(([k, v]) => (node[k] = v));
+    }
+    if (e.children) {
+        const fragment = document.createDocumentFragment();
+        for (const child of compact(e.children)) {
+            fragment.appendChild(createElement(child, namespace));
+        }
+        node.appendChild(fragment);
+    }
+    return node;
+}
+
+class Elem {
+    constructor(obj) {
+        if (!obj) return;
+
+        if (obj instanceof Elem || obj instanceof Array) {
+            this.children = obj;
+        } else if (typeof obj === "string" || typeof obj === "number") {
+            this.innerText = obj;
+        } else {
+            Object.assign(this, obj);
+        }
+    }
+
+    static with(obj = {}) {
+        return new this(obj);
+    }
+
+    and(obj) {
+        const updated = Object.create(Object.getPrototypeOf(this));
+        return deepMerge(Object.assign(updated, this), obj);
+    }
+
+    create({ profile: profileName, namespace } = {}) {
+        return profile(profileName, () => createElement(this, namespace));
+    }
+
+    wrapWith(element, options = {}) {
+        if (!options?.optional) {
+            return this;
+        }
+        element.children = Array.isArray(element.children)
+            ? element.children
+            : element.children
+              ? [element.children]
+              : [];
+        element.children.splice(
+            options?.position === undefined ? element.children.length : options.position,
+            0,
+            this,
+        );
+        return element;
+    }
+
+    addChild(child) {
+        if (child) {
+            this.children = Array.isArray(this.children)
+                ? this.children
+                : this.children
+                  ? [this.children]
+                  : [];
+            this.children.push(child);
+        }
+        return this;
+    }
+}
+
 export class Dom {
     static formatKeys = (obj, format = kebabCase) =>
         Object.entries(obj)
@@ -27,52 +129,6 @@ export class Dom {
             return target[key] ?? target.getElementById(key);
         },
     });
-
-    static createElement(e, namespace) {
-        namespace = namespace || e.namespace;
-        const type = e.constructor.name.replace("_", "").toLowerCase();
-        const node = namespace
-            ? document.createElementNS(namespace, type)
-            : document.createElement(type);
-
-        e.node = node;
-        node.source = e;
-
-        if (e.id) node.id = e.id;
-        if (e.innerText !== undefined) node.textContent = e.innerText;
-        if (e.text !== undefined) node.textContent = e.text;
-        if (e.value !== undefined) node.value = e.value;
-        if (e.type) node.type = e.type;
-        if (e.editable) node.contentEditable = e.editable;
-
-        if (e.class) {
-            node.className = compact(e.class).join(" ").trim();
-        }
-        if (e.event) {
-            for (let k in e.event) {
-                node.addEventListener(k, e.event[k].bind(node, node), false);
-            }
-        }
-        if (e.attribute) {
-            Dom.formatKeys(e.attribute).forEach(([k, v]) => node.setAttribute(k, v));
-        }
-        if (e.style) {
-            Dom.formatKeys(e.style).forEach(([k, v]) =>
-                node.style.setProperty(k, ...(v.split ? v.split("!") : [v])),
-            );
-        }
-        if (e.function) {
-            Object.entries(e.function).forEach(([k, v]) => (node[k] = v));
-        }
-        if (e.children) {
-            const fragment = document.createDocumentFragment();
-            for (const child of compact(e.children)) {
-                fragment.appendChild(Dom.createElement(child, namespace));
-            }
-            node.appendChild(fragment);
-        }
-        return node;
-    }
 
     static addStyleNode = (css) =>
         document.head.appendChild(
@@ -106,50 +162,6 @@ export class Dom {
         observer.observe(parent, { childList: true, subtree: true });
         document.querySelectorAll(targetSelector).forEach((element) => callback(element, observer));
     }
-
-    static Elem = class {
-        static with(obj) {
-            return Object.assign(new this(), obj);
-        }
-
-        and(obj) {
-            const updated = Object.create(Object.getPrototypeOf(this));
-            return deepMerge(Object.assign(updated, this), obj);
-        }
-
-        create({ profile: profileName, namespace } = {}) {
-            return profile(profileName, () => Dom.createElement(this, namespace));
-        }
-
-        wrapWith(element, options = {}) {
-            if (!options?.optional) {
-                return this;
-            }
-            element.children = Array.isArray(element.children)
-                ? element.children
-                : element.children
-                  ? [element.children]
-                  : [];
-            element.children.splice(
-                options?.position === undefined ? element.children.length : options.position,
-                0,
-                this,
-            );
-            return element;
-        }
-
-        addChild(child) {
-            if (child) {
-                this.children = Array.isArray(this.children)
-                    ? this.children
-                    : this.children
-                      ? [this.children]
-                      : [];
-                this.children.push(child);
-            }
-            return this;
-        }
-    };
 
     static svgCamelCaseAttributes = [
         "allowReorder",
@@ -220,77 +232,77 @@ export class Dom {
 }
 
 /* TextElements */
-export class Div extends Dom.Elem {}
-export class Span extends Dom.Elem {}
-export class P extends Dom.Elem {}
-export class Small extends Dom.Elem {}
-export class Menu extends Dom.Elem {}
-export class A extends Dom.Elem {}
+export class Div extends Elem {}
+export class Span extends Elem {}
+export class P extends Elem {}
+export class Small extends Elem {}
+export class Menu extends Elem {}
+export class A extends Elem {}
 /* HeadingElements */
-export class H1 extends Dom.Elem {}
-export class H2 extends Dom.Elem {}
-export class H3 extends Dom.Elem {}
-export class H4 extends Dom.Elem {}
-export class H5 extends Dom.Elem {}
-export class H6 extends Dom.Elem {}
-export class HGroup extends Dom.Elem {}
+export class H1 extends Elem {}
+export class H2 extends Elem {}
+export class H3 extends Elem {}
+export class H4 extends Elem {}
+export class H5 extends Elem {}
+export class H6 extends Elem {}
+export class HGroup extends Elem {}
 /* InlineTextElements */
-export class I extends Dom.Elem {}
-export class Strong extends Dom.Elem {}
-export class Em extends Dom.Elem {}
-export class Mark extends Dom.Elem {}
-export class Abbr extends Dom.Elem {}
-export class Code extends Dom.Elem {}
-export class Pre extends Dom.Elem {}
-export class Kbd extends Dom.Elem {}
-export class Samp extends Dom.Elem {}
+export class I extends Elem {}
+export class Strong extends Elem {}
+export class Em extends Elem {}
+export class Mark extends Elem {}
+export class Abbr extends Elem {}
+export class Code extends Elem {}
+export class Pre extends Elem {}
+export class Kbd extends Elem {}
+export class Samp extends Elem {}
 /* SectionElements */
-export class Header extends Dom.Elem {}
-export class Footer extends Dom.Elem {}
-export class Article extends Dom.Elem {}
-export class Aside extends Dom.Elem {}
-export class Section extends Dom.Elem {}
+export class Header extends Elem {}
+export class Footer extends Elem {}
+export class Article extends Elem {}
+export class Aside extends Elem {}
+export class Section extends Elem {}
 /* ListElements */
-export class UL extends Dom.Elem {}
-export class OL extends Dom.Elem {}
-export class LI extends Dom.Elem {}
-export class Nav extends Dom.Elem {}
+export class UL extends Elem {}
+export class OL extends Elem {}
+export class LI extends Elem {}
+export class Nav extends Elem {}
 /* FormElements */
-export class Form extends Dom.Elem {}
-export class Input extends Dom.Elem {}
-export class Output extends Dom.Elem {}
-export class TextArea extends Dom.Elem {}
-export class Button extends Dom.Elem {}
-export class Label extends Dom.Elem {}
-export class Select extends Dom.Elem {}
-export class Option_ extends Dom.Elem {}
+export class Form extends Elem {}
+export class Input extends Elem {}
+export class Output extends Elem {}
+export class TextArea extends Elem {}
+export class Button extends Elem {}
+export class Label extends Elem {}
+export class Select extends Elem {}
+export class Option_ extends Elem {}
 /* MediaElements */
-export class Img extends Dom.Elem {}
-export class Audio_ extends Dom.Elem {}
-export class Video extends Dom.Elem {}
+export class Img extends Elem {}
+export class Audio_ extends Elem {}
+export class Video extends Elem {}
 /* TableElements */
-export class Table extends Dom.Elem {}
-export class TR extends Dom.Elem {}
-export class TD extends Dom.Elem {}
-export class TH extends Dom.Elem {}
-export class TBody extends Dom.Elem {}
-export class TFoot extends Dom.Elem {}
-export class THead extends Dom.Elem {}
-export class ColGroup extends Dom.Elem {}
-export class Col extends Dom.Elem {}
-export class Caption extends Dom.Elem {}
+export class Table extends Elem {}
+export class TR extends Elem {}
+export class TD extends Elem {}
+export class TH extends Elem {}
+export class TBody extends Elem {}
+export class TFoot extends Elem {}
+export class THead extends Elem {}
+export class ColGroup extends Elem {}
+export class Col extends Elem {}
+export class Caption extends Elem {}
 /* EmbeddedElements */
-export class IFrame extends Dom.Elem {}
-export class Canvas extends Dom.Elem {}
+export class IFrame extends Elem {}
+export class Canvas extends Elem {}
 /* MiscElements */
-export class HR extends Dom.Elem {}
-export class BR extends Dom.Elem {}
-export class Style extends Dom.Elem {}
-export class Blockquote extends Dom.Elem {}
-export class Cite extends Dom.Elem {}
-export class Sup extends Dom.Elem {}
-export class Sub extends Dom.Elem {}
+export class HR extends Elem {}
+export class BR extends Elem {}
+export class Style extends Elem {}
+export class Blockquote extends Elem {}
+export class Cite extends Elem {}
+export class Sup extends Elem {}
+export class Sub extends Elem {}
 /* HeadElements */
-export class Link extends Dom.Elem {}
-export class Title extends Dom.Elem {}
-export class Meta extends Dom.Elem {}
+export class Link extends Elem {}
+export class Title extends Elem {}
+export class Meta extends Elem {}
